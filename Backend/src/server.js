6575dcf24cd66871e34eadfa0cd218787ede3dae
SathtikBose <sathtikbose@gmail.com>
2026-04-13 11:50:00 +0530

    bugs fixed for railway hosting

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.listen(PORT, "0.0.0.0", () => {
-  `Server is running on port ${PORT}`;
+  console.log(`Server is running on port ${PORT}`);
 });
