0c4f691bc78b31e96f072e7ba6c7c5213d1c2f04
SathtikBose <sathtikbose@gmail.com>
2026-04-15 11:19:32 +0530

    feat : (_backend) removed self ping

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -9,20 +9,5 @@ connectDB();
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`server started on port ${PORT}`);
-  startSelfPing();
 });
 
-function startSelfPing() {
-  const renderUrl = process.env.RENDER_URL;
-  if (!renderUrl) return;
-
-  const client = renderUrl.startsWith("https") ? https : http;
-
-  setInterval(
-    () => {
-      client.get(`${renderUrl}/health`, () => {}).on("error", () => {});
-      console.log(`hello health check`);
-    },
-    2 * 60 * 1000,
-  );
-}
